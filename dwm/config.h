4d5f802d1c9c012837527d7de911745bbfa23f0d
Luma <hydroxycult@gmail.com>
2026-01-02 13:23:00 +0530

    Update config.h

--- dwm/config.h
+++ dwm/config.h
@@ -37,7 +37,7 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "com.lite_xl.LiteXL",  NULL,  NULL,  1 << 0,  0,  -1 },
+	{ "com.lite_xl.LiteXL",  NULL,  NULL,  1 << 8,  0,  -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
